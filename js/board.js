b2b4b2bd151121e0500fa269186a236bfd566380
Ankit Ydv <ankityadav11241@gmail.com>
2024-03-06 22:28:47 +0530

    canvas setup & strokesColor

--- js/board.js
+++ js/board.js
@@ -1,21 +1,41 @@
-const board = document.getElementById("board");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+let isDrawing = false;
+let strokeColor = "black";
 
-board.addEventListener("mousedown", (e) => {
-  let x = e.clientX,
-    y = e.clientY;
-  createDot(x, y);
-  addEventListener("drag", (e) => {
-    let x = e.clientX,
-      y = e.clientY;
-    createDot(x, y);
-  });
-});
+// Set canvas size to match screen size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const createDot = (xCoord, yCoord) => {
-  console.log(xCoord, yCoord);
-  const dot = document.createElement("span");
-  dot.classList.add("dot");
-  dot.style.left = xCoord - 5 + "px";
-  dot.style.top = (yCoord - board.getBoundingClientRect().top - 5) + "px";
-  board.appendChild(dot);
-};
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+
+function startDrawing(event) {
+  isDrawing = true;
+  draw(event);
+}
+
+function changeStrokeColor(color) {
+  strokeColor = color;
+}
+
+function draw(event) {
+  if (!isDrawing) return;
+
+  // Adjust mouse coordinates to match canvas size
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  context.lineWidth = 5;
+  context.strokeStyle = strokeColor;
+
+  context.lineTo(x, y);
+  context.stroke();
+}
+
+function stopDrawing() {
+  isDrawing = false;
+  context.beginPath();
+}
